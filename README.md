c3866f85216e10566ef1a53e25b4c9cbfd569e2d
mikerooksby <58581550+mikerooksby@users.noreply.github.com>
2022-11-11 11:21:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent-of-Vue-Pilot
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/aov-2022-pilot-puzzle-fbz5jg)
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,vue,ts}'],
+  theme: {
+    colors: {
+      green: '#42b883',
+      'gray-dark': '#0f172a',
+    },
+  },
+}
